f35c72a2100aa2abf8be7a78e5882c39671d041f
yaojinhui <yaojinhui1993@gmail.com>
2018-11-08 11:01:22 +0800

    Updates

---
+++ precache-manifest.80b3eec9e889b62346db2b6bff4cf071.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "fde72f966578a5308888",
+    "url": "/react-tutorial/static/js/main.fde72f96.chunk.js"
+  },
+  {
+    "revision": "1670935a042b1fdc38db",
+    "url": "/react-tutorial/static/js/1.1670935a.chunk.js"
+  },
+  {
+    "revision": "fde72f966578a5308888",
+    "url": "/react-tutorial/static/css/main.b7c1f213.chunk.css"
+  },
+  {
+    "revision": "06e979690ebe9b1901eaaa6434819fa2",
+    "url": "/react-tutorial/index.html"
+  }
+];
